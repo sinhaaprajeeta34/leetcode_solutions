b0a9271326644edb155e22b40865c8f8c9624d60
Aprajeeta Sinha <sinhaaprajeeta34@gmail.com>
2026-08-18 23:14:15 +0530

    Time: 3 ms (14.35%), Space: 12.5 MB (5.52%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        vector<int> vec(2001,0);
+
+        for(int &x : arr){
+            vec[x + 1000]++;
+        }
+        sort(begin(vec), end(vec));
+        for(int i=1; i<2001;i++){
+            if(vec[i]!=0 && vec[i]==vec[i-1])
+            return false;
+        }
+           return true;
+    }
+};
