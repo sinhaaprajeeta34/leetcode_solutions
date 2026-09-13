66e00dac7911f1108b09deeaae3430f676d2b53e
Aprajeeta Sinha <sinhaaprajeeta34@gmail.com>
2026-09-13 22:04:01 +0530

    Time: 58 ms (43.5%), Space: 12.7 MB (98.21%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        int ans = 0;
+
+        // Shift img2 by (dr, dc)
+        for (int dr = -n + 1; dr < n; dr++) {
+            for (int dc = -n + 1; dc < n; dc++) {
+
+                int overlap = 0;
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+
+                        int x = i + dr;
+                        int y = j + dc;
+
+                        if (x >= 0 && x < n &&
+                            y >= 0 && y < n) {
+
+                            if (img1[i][j] == 1 && img2[x][y] == 1)
+                                overlap++;
+                        }
+                    }
+                }
+
+                ans = max(ans, overlap);
+            }
+        }
+
+        return ans;
+    }
+};
